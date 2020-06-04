92fd32cb97989490c0cbb644111f61c1f76cdef8
Jacob Corum <cor15031@byui.edu>
2020-06-03 21:40:54 -0600

    language feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term"); 
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -78,3 +79,27 @@ var displayRepos = function(repos, searchTerm) {
         repoEl.appendChild(statusEl);
     }
 };
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.JSON().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    }
+    else {
+      alert("Error: " + response.statusText)
+    }
+  });
+};
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+  }
+  repoContainerEl.textContent = "";
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
